0eacb627c8dd0a13750d52ac85250c7bd603a4ee
piyushsarkar <piyushsarkar@users.noreply.github.com>
2020-04-21 08:11:01 +0530

    Removed unnecessary code

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,15 +1,6 @@
 @extends('layouts.layout')
 
 @section('content')
-    <!-- @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif -->
 <div class="container registration-form">
     <form action="/" method="POST">
     @csrf
@@ -21,21 +12,21 @@
         @else
         <div class="row">
             <div class="col-md-12">
-                <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+                <div class="form-group">
                     <label for="name" class="control-label">Name:</label>
                     <input type="text" id="name" name="name" class="form-control" value="{{ old('name') }}">
                     @if ($errors->has('name'))
                         <span class="text-danger">{{ $errors->first('name') }}</span>
                     @endif
                 </div>
-                <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                <div class="form-group">
                     <label for="name" class="control-label">Email:</label>
                     <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}">
                     @if ($errors->has('email'))
                         <span class="text-danger">{{ $errors->first('email') }}</span>
                     @endif
                 </div>
-                <div class="form-group {{ $errors->has('pincode') ? 'has-error' : '' }}">
+                <div class="form-group">
                     <label for="name" class="control-label">Pincode:</label>
                     <input type="text" id="pincode" name="pincode" class="form-control" maxlength="6" value="{{ old('pincode') }}">
                     @if ($errors->has('pincode'))
